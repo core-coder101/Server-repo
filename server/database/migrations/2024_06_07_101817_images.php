d48cae26b99e4ab2635954d3d13a41a4ebbf4910
Ahmad Mujtaba Saeed <ahmadmujtabap70@gmail.com>
2024-06-07 04:32:50 -0700

    newOneFriday

--- server/database/migrations/2024_06_07_101817_images.php
+++ server/database/migrations/2024_06_07_101817_images.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -19,9 +18,9 @@ return new class extends Migration
             $table->string('ImageName');
             $table->timestamps();
             $table->foreign('UsersID')
-                  ->references('id')
-                  ->on('users')
-                  ->onDelete('cascade');
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
